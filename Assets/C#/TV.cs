5e66e81599671025f146136a087bbe11ac9bed22
Simon <mmm998520@gmail.com>
2020-02-01 16:40:23 +0800

    TV animation

---
+++ Assets/C#/TV.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;   
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TV : MonoBehaviour
+{
+    public VideoClip[] clip = new VideoClip[10];
+    public VideoPlayer VP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VP = gameObject.GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((ulong)VP.frame >= VP.frameCount-2 && (ulong)VP.frame<1000)
+        {
+            print("1");
+            VideoClip temp = VP.clip;
+            while(VP.clip == temp)
+            {
+                VP.clip = clip[Random.Range(0, clip.Length)];
+                VP.frame = 1;
+            }
+            print(VP.clip);
+        }
+    }
+}
