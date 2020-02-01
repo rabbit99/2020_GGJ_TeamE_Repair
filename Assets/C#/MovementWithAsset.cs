1cdc6a4f88065bc053d899c9571978bad425b3fe
vvbest2012 <vvbest2012@gmail.com>
2020-02-01 15:19:04 +0800

    notification

--- Assets/C#/MovementWithAsset.cs
+++ Assets/C#/MovementWithAsset.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementWithAsset : MonoBehaviour
+public class MovementWithAsset : MonoBehaviour, INotification
+
 {
     public float speed = 1;
     private NewControls newControls = null;
@@ -15,11 +16,12 @@ public class MovementWithAsset : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddNotificationObserver();
+        //NotificationCenter.Default.Post(this, NotificationKeys.MissionInfoRefresh);
     }
 
     // Update is called once per frame
-    
+
 
     private void Move() {
         var mInput = newControls.Newactionmap.Newaction.ReadValue<Vector2>();
@@ -32,4 +34,43 @@ public class MovementWithAsset : MonoBehaviour
         //Debug.Log(" mInput.x" + mInput.x);
         transform.Translate(moveMent * speed * Time.deltaTime);
     }
+
+    #region Notification
+    void AddNotificationObserver()
+    {
+        //NotificationCenter.Default.AddObserver(this, NotificationKeys.LeaveShop);
+        //NotificationCenter.Default.AddObserver(this, NotificationKeys.StageCountChange);
+        //NotificationCenter.Default.AddObserver(this, NotificationKeys.QueryStagesFinished);
+    }
+
+    void RemoveNotificationObserver()
+    {
+        //NotificationCenter.Default.RemoveObserver(this, NotificationKeys.LeaveShop);
+        //NotificationCenter.Default.RemoveObserver(this, NotificationKeys.StageCountChange);
+        //NotificationCenter.Default.RemoveObserver(this, NotificationKeys.QueryStagesFinished);
+    }
+
+    public void OnNotify(Notification _noti)
+    {
+        //if (_noti.name == NotificationKeys.StageCountChange)
+        //{
+        //    //Debug.Log("StageCountChange " + (int)_noti.data);
+        //    InputStageCount_ValueChanged((int)_noti.data);
+
+        //}
+        //else if (_noti.name == NotificationKeys.LeaveShop)
+        //{
+        //    mSetStage.RefreshLimit();
+        //    moneyLabel.text = Main.instance.ParseDataMgr.userData.Money.ToString();
+
+        //}
+        //else if (_noti.name == NotificationKeys.QueryStagesFinished)
+        //{
+        //    //更新stage count ui
+        //    //Debug.Log("QueryStagesFinished " + (int)_noti.data);
+        //    InputStageCount_ValueChanged((int)_noti.data);
+        //    isSaveAsDraft = true;
+        //}
+    }
+    #endregion
 }
