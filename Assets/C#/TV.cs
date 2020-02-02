e0868bdd33884c7bc323237bc6e126aa1d64e37e
vvbest2012 <vvbest2012@gmail.com>
2020-02-02 12:49:55 +0800

    update TV logic. recive event to check play anim.

--- Assets/C#/TV.cs
+++ Assets/C#/TV.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 
-public class TV : MonoBehaviour
+public class TV : MonoBehaviour, INotification
 {
     public VideoClip[] clip = new VideoClip[10];
     public VideoPlayer VP;
+    public VideoClip MonsterHurtClip;
+    public VideoClip RobotHurtClip;
+
+    private bool playResult = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,8 @@ public class TV : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (playResult)
+            return;
         if ((ulong)VP.frame >= VP.frameCount-10 && (ulong)VP.frame<1000)
         {
             print("1");
@@ -28,4 +34,61 @@ public class TV : MonoBehaviour
             print(VP.clip);
         }
     }
+
+    private void PlayMonsterHurtClip() {
+        if (MonsterHurtClip == null)
+            return;
+        VP.clip = MonsterHurtClip;
+        VP.frame = 1;
+        StartCoroutine(callBack());
+    }
+
+    private void PlayRobotHurtClip()
+    {
+        if (RobotHurtClip == null)
+            return;
+        VP.clip = RobotHurtClip;
+        VP.frame = 1;
+        StartCoroutine(callBack());
+    }
+
+    IEnumerator callBack()
+    {
+        while ((ulong)VP.frame >= VP.frameCount - 10 && (ulong)VP.frame < 1000) // while the movie is playing
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        // after movie is not playing / has stopped.
+        setFlag();
+    }
+
+    private void setFlag()
+    {
+        playResult = false;
+    }
+
+    #region Notification
+    void AddNotificationObserver()
+    {
+        NotificationCenter.Default.AddObserver(this, NotificationKeys.MonsterHurt);
+        NotificationCenter.Default.AddObserver(this, NotificationKeys.RobotHurt);
+    }
+
+    void RemoveNotificationObserver()
+    {
+        NotificationCenter.Default.RemoveObserver(this, NotificationKeys.MonsterHurt);
+        NotificationCenter.Default.RemoveObserver(this, NotificationKeys.RobotHurt);
+    }
+
+    public void OnNotify(Notification _noti)
+    {
+        if (_noti.name == NotificationKeys.InTheLadder)
+        {
+            playResult = true;
+        }
+        if (_noti.name == NotificationKeys.InTheLadder)
+        {
+        }
+    }
+    #endregion
 }
